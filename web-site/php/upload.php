df23ca6f193a89131681cd9a7ba99b6fdf81e068
OlivierLD <olivier@lediouris.net>
2024-02-22 08:40:05 +0100

    File upload

--- web-site/php/upload.php
+++ web-site/php/upload.php
@@ -1,4 +1,14 @@
 <?php
+// Raw result of the upload operation
+// The name "fileToUpload" comes from the submitted form.
+$array = $_FILES["fileToUpload"];
+// echo "Received: [" . $_FILES["fileToUpload"] . "]<br/>" . PHP_EOL;
+echo 'Received:<br/>' . PHP_EOL;
+echo '<pre>' . PHP_EOL; 
+print_r($array); 
+echo '</pre>' . PHP_EOL;
+echo '<br/>' . PHP_EOL;
+
 $target_dir = "uploads/"; // Under THIS current directory
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;  // ie true
@@ -8,41 +18,41 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 if (isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if ($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "File is an image - " . $check["mime"] . ".<br/>" . PHP_EOL;
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "File is not an image.<br/>" . PHP_EOL;
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "Sorry, file already exists.<br/>" . PHP_EOL;
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > (1 * 1024 * 1024)) {  // ~1 Mb
-  echo "Sorry, your file is too large (it's (" . $_FILES["fileToUpload"]["size"] . " big).";
+  echo "Sorry, your file is too large (it's (" . $_FILES["fileToUpload"]["size"] . " big).<br/>" . PHP_EOL;
   $uploadOk = 0;
 }
 
 // Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br/>" . PHP_EOL;
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo "Sorry, your file was not uploaded.<br/>" . PHP_EOL;
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])). " has been uploaded.<br/>" . PHP_EOL;
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo "Sorry, there was an error uploading your file.<br/>" . PHP_EOL;
   }
 }
 ?>
