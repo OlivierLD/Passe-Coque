d2a7ffaf73a1d1518ac1b36bbe228cc9f4ed6406
OlivierLD <olivier@lediouris.net>
2024-04-15 14:30:50 +0200

    Cleanup, DB

--- web-site/admin/sql/create.tables.sql
+++ web-site/admin/sql/create.tables.sql
@@ -267,3 +267,58 @@ CREATE TABLE IF NOT EXISTS THE_FLEET (
     CATEGORY VARCHAR(16) DEFAULT 'NONE' CHECK (CATEGORY IN ('CLUB', 'TO_GRAB', 'EX_BOAT', 'NONE')),  -- "TO_GRAB",
     BASE VARCHAR(64)            -- "Saint&#8209;Philibert"
 );
+
+CREATE TABLE IF NOT EXISTS REFERENTS (
+    EMAIL VARCHAR(64) NOT NULL,
+    BOAT_ID VARCHAR(64) NOT NULL,
+    TELEPHONE VARCHAR(32),
+    CONSTRAINT REFERENTS_PK PRIMARY KEY(EMAIL, BOAT_ID),
+    CONSTRAINT REFERENTS_FK_MEMBERS FOREIGN KEY (EMAIL) REFERENCES PASSE_COQUE_MEMBERS (EMAIL) ON DELETE CASCADE,
+    CONSTRAINT REFERENTS_FK_BOATS FOREIGN KEY (BOAT_ID) REFERENCES THE_FLEET (ID) ON DELETE CASCADE
+);
+
+SELECT EMAIL, FIRST_NAME, LAST_NAME, TELEPHONE FROM PASSE_COQUE_MEMBERS WHERE UPPER(LAST_NAME) LIKE '%ALLAIS%';
+
+CREATE VIEW BOATS_AND_REFERENTS AS
+SELECT M.EMAIL, M.FIRST_NAME, M.LAST_NAME,
+       B.BOAT_NAME, B.BOAT_TYPE,
+       R.BOAT_ID, R.TELEPHONE
+FROM PASSE_COQUE_MEMBERS M,
+     THE_FLEET B,
+     REFERENTS R
+WHERE R.BOAT_ID = B.ID AND
+      R.EMAIL = M.EMAIL;       
+       
+INSERT INTO REFERENTS VALUES ('benjamin.rasseneur@laposte.net', 'ar-mor-van', 'unknown');
+-- INSERT INTO REFERENTS VALUES ('smenuet@evoceane.fr', 'babou', 'unknown'); -- Not found in members
+INSERT INTO REFERENTS VALUES ('2l2telecnav@gmail.com', 'coevic-2', '+33 7 56 85 44 38');
+INSERT INTO REFERENTS VALUES ('lepecheur.g@gmail.com', 'coraxy', 'unknown');
+INSERT INTO REFERENTS VALUES ('jeff.allais@hotmail.fr', 'eh-tak', '+33 6 35 59 18 80');
+INSERT INTO REFERENTS VALUES ('marmajour@gmail.com', 'gwenillig', 'unknown');
+INSERT INTO REFERENTS VALUES ('isseo.asso@gmail.com', 'imagine', 'unknown');
+INSERT INTO REFERENTS VALUES ('patrick.potiron1@orange.fr', 'jolly-jumper', 'unknown');
+INSERT INTO REFERENTS VALUES ('garciaalain299@gmail.com', 'jolly-jumper', 'unknown');
+INSERT INTO REFERENTS VALUES ('l.brochard.archi@gmail.com', 'heure-bleue', 'unknown');
+INSERT INTO REFERENTS VALUES ('yann.legrand56@icloud.com', 'la.reveuse' , '+33 6 75 00 80 15');
+INSERT INTO REFERENTS VALUES ('alain.hahusseau@sfr.fr', 'la.reveuse' , '+33 7 88 72 41 71');
+INSERT INTO REFERENTS VALUES ('alain.hahusseau@sfr.fr', 'ma-enez' , '+33 7 88 72 41 71');
+INSERT INTO REFERENTS VALUES ('germain.regis@orange.fr', 'manu-oviri', '+33 6 83 58 47 43');
+INSERT INTO REFERENTS VALUES ('tiagocampostc@hotmail.com', 'melkart', 'unknown');
+INSERT INTO REFERENTS VALUES ('bouan.g@gmail.com', 'melvan', '+33 6 34 33 44 71');
+INSERT INTO REFERENTS VALUES ('andres.juan@wanadoo.fr', 'mirella', 'unknown');  -- Jean Larie ?
+INSERT INTO REFERENTS VALUES ('marine.prevet@atelierkopal.fr', 'passpartout', 'unknown');
+INSERT INTO REFERENTS VALUES ('contact@voyageenpatrimoine.fr', 'passpartout', 'unknown');
+INSERT INTO REFERENTS VALUES ('421jimmypahun@gmail.com', 'pordin-nancq', '+33 6 14 11 20 09');
+-- Referent Saigane ?
+-- Referent Saudade ?
+INSERT INTO REFERENTS VALUES ('mlemeni@yahoo.fr', 'stiren', '+33 7 77 60 78 89');
+INSERT INTO REFERENTS VALUES ('morisseauguillaume@msn.com', 'taapuna', 'unknown');
+INSERT INTO REFERENTS VALUES ('bmenuet@wanadoo.fr', 'tokad-2', 'unknown');
+INSERT INTO REFERENTS VALUES ('litimat1@free.fr', 'trehudal', '+33 6 62 34 88 86');
+-- INSERT INTO REFERENTS VALUES ('smenuet@evoceane.fr', 'tri-yann', 'unknown'); -- Not in members... email address.
+-- Twist again ?
+-- Wanita Too ?
+
+SELECT * FROM BOATS_AND_REFERENTS ORDER BY BOAT_ID;
+
+-- TODO BOAT_CLUB_MEMBERS
