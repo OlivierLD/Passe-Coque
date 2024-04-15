7dc6443f93cf51d91ff6b6ed94fd6a81db0a2854
OlivierLD <olivier@lediouris.net>
2024-04-15 17:34:56 +0200

    Referents management

---
+++ web-site/admin/sql/_referents.01.php
@@ -0,0 +1,138 @@
+<html lang="en">
+  <!--
+   ! Access to the REFERENTS Table
+   +-->
+  <head>
+    <!--meta charset="UTF-8">
+    <meta charset="ISO-8859-1"-->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Referents - Members, Fleet</title>
+    <style type="text/css">
+      * {
+        font-family: 'Courier New'
+      }
+
+      tr > td {
+        border: 1px solid silver;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>PHP / MySQL. Passe-Coque Referents. Depends on Members and Fleet</h1>
+
+    <?php
+// phpinfo();
+
+require __DIR__ . "/../../php/db.cred.php";
+
+// Change to db.cred
+// $username = "passecc128";
+// $password = "zcDmf7e53eTs";
+// $database = "passecc128";
+// $dbhost = "passecc128.mysql.db";
+
+if (isset($_POST['operation'])) {
+  $operation = $_POST['operation'];
+  if ($operation == 'query') { // Then do the query
+    try {
+      $boat_name = $_POST['boat-name']; 
+      $ref_name = $_POST['ref-name'];  
+
+      // $link = mysqli_init();  // Mandatory ?
+    
+      echo("Will connect on ".$database." ...<br/>");
+      $link = new mysqli($dbhost, $username, $password, $database);
+    
+      if ($link->connect_errno) {
+        echo("Oops, errno:".$link->connect_errno."...<br/>");
+        die("Connection failed: " . $conn->connect_error);
+      } else {
+        echo("Connected.<br/>");
+      }
+    
+      $sql = 'SELECT EMAIL, 
+                     FIRST_NAME, 
+                     LAST_NAME, 
+                     BOAT_NAME, 
+                     BOAT_TYPE, 
+                     BOAT_ID, 
+                     TELEPHONE 
+              FROM BOATS_AND_REFERENTS 
+              WHERE (UPPER(BOAT_NAME) LIKE UPPER(\'%' . $boat_name . '%\') OR
+                    UPPER(BOAT_ID) LIKE UPPER(\'%' . $boat_name . '%\')) AND (
+                    UPPER(FIRST_NAME) LIKE UPPER(\'%' . $ref_name . '%\') OR
+                    UPPER(LAST_NAME) LIKE UPPER(\'%' . $ref_name . '%\') OR
+                    UPPER(EMAIL) LIKE UPPER(\'%' . $ref_name . '%\'))
+              ORDER BY 1;';
+      
+      echo('Performing query <code>' . $sql . '</code><br/>');
+    
+      // $result = mysql_query($sql, $link);
+      $result = mysqli_query($link, $sql);
+      echo ("Returned " . $result->num_rows . " row(s)<br/>");
+    
+      echo("<h2>Passe-Coque Referents</h2>");
+
+      ?> <!-- Member creation form -->
+
+      <form action="./_referents.02.php" method="get">
+        <input type="hidden" name="task" value="create">
+        <input type="submit" value="Create New Referent">
+      </form> 
+
+      <?php
+
+
+      echo "<table>";
+      echo "<tr><th>Email</th><th>First Name</th><th>Last Name</th><th>Boat Name</th><th>Boat Type</th><th>Boat ID</th><th>Ref. Tel.</th><th>-</th></tr>";
+      //             0             1                  2                 3                 4                 5               6
+      while ($table = mysqli_fetch_array($result)) { // go through each row that was returned in $result
+        echo(
+          "<tr><td>" . 
+            urldecode($table[0]) . "</td><td>" . urldecode($table[1]) . "</td><td>" . urldecode($table[2]) . "</td><td>" . urldecode($table[3]) . "</td><td>" . urldecode($table[4]) . "</td><td>" . urldecode($table[5]) . "</td><td>" . ($table[6]) . "</td><td>" . "<a href='./_referents.02.php?email=" . $table[0] . "&boat-id=" . $table[5] . "' target='RefUpdate'>Edit</a>" .
+          "</td></tr>\n"
+        ); 
+      }
+      echo "</table>";
+      
+      // On ferme !
+      $link->close();
+      echo("Closed DB<br/>".PHP_EOL);
+    } catch (Throwable $e) {
+      echo "Captured Throwable for connection : " . $e->getMessage() . "<br/>" . PHP_EOL;
+    }
+    echo("<hr/>" . PHP_EOL);
+    // echo("Again ? Click <a href='#'>Here</a>.");
+    ?>
+    <form action="./_referents.01.php" method="get">
+      <!--input type="hidden" name="operation" value="blank"-->
+      <table>
+        <tr>
+          <td colspan="2" style="text-align: center;"><input type="submit" value="Query Again ?"></td>
+        </tr>
+      </table>
+    </form>
+    <?php
+  }
+} else { // Then display the query form
+    ?>
+    <form action="#" method="post">
+      <input type="hidden" name="operation" value="query">
+      <table>
+        <tr>
+          <td valign="top">Boat (part of Name or ID):</td><td><input type="text" name="boat-name" size="40"></td>
+        </tr>
+        <tr>
+          <td valign="top">Referent (part of first or last name, email):</td><td><input type="text" name="ref-name" size="40"></td>
+        </tr>
+        <tr>
+          <td colspan="2" style="text-align: center;"><input type="submit" value="Query"></td>
+        </tr>
+      </table>
+    </form>
+    <?php
+}  
+    ?>        
+  </body>        
+</html>
---
+++ web-site/admin/sql/_referents.02.php
@@ -0,0 +1,340 @@
+<html lang="en">
+  <!--
+   ! A Form to Update the REFERENTS table, leads to an update form.
+   +-->
+  <head>
+    <!--meta charset="UTF-8">
+    <meta charset="ISO-8859-1"-->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Edit Referents</title>
+    <style type="text/css">
+      * {
+        font-family: 'Courier New'
+      }
+
+      tr > td {
+        border: 1px solid silver;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>PHP / MySQL. Passe-Coque Referents</h1>
+
+    <?php
+// phpinfo();
+
+$VERBOSE = false;
+
+require __DIR__ . "/../../php/db.cred.php";
+
+class Boat {
+  public $id;
+  public $name;
+  public $type;
+}
+
+class Referent {
+  public $email;
+  public $firstName;
+  public $lastName;
+}
+
+function getBoats(string $dbhost, string $username, string $password, string $database, bool $verbose): array {
+  try {
+      $link = new mysqli($dbhost, $username, $password, $database);
+      
+      if ($link->connect_errno) {
+          echo("[Oops, errno:".$link->connect_errno."...] ");
+          // die("Connection failed: " . $conn->connect_error);
+          throw $conn->connect_error;
+      } else {
+          if ($verbose) {
+              echo("[Connected.] ");
+          }
+      }
+      $sql = "SELECT ID, BOAT_NAME, BOAT_TYPE FROM THE_FLEET;";
+      if ($verbose) {
+          echo('[Performing instruction ['.$sql.']] ');
+      }
+      
+      $result = mysqli_query($link, $sql);
+      if ($verbose) {
+          echo ("Returned " . $result->num_rows . " row(s)<br/>");
+      }
+
+      $boats = array();
+      $boatIndex = 0;
+      while ($table = mysqli_fetch_array($result)) { // go through each row that was returned in $result
+          $boats[$boatIndex] = new Boat();
+          $boats[$boatIndex]->id = $table[0];
+          $boats[$boatIndex]->name = $table[1];
+          $boats[$boatIndex]->type = $table[2];
+          $boatIndex++;
+      }
+      // On ferme !
+      $link->close();
+      if ($verbose) {
+          echo("[Closed DB] ".PHP_EOL);
+          echo "Finally, returning $boats";
+      }
+      return $boats;
+
+  } catch (Throwable $e) {
+      echo "[ Captured Throwable for connection : " . $e->getMessage() . "] " . PHP_EOL;
+      throw $e;
+  }                
+  return null;
+}
+
+function getMembers(string $dbhost, string $username, string $password, string $database, bool $verbose): array {
+  try {
+      $link = new mysqli($dbhost, $username, $password, $database);
+      
+      if ($link->connect_errno) {
+          echo("[Oops, errno:".$link->connect_errno."...] ");
+          // die("Connection failed: " . $conn->connect_error);
+          throw $conn->connect_error;
+      } else {
+          if ($verbose) {
+              echo("[Connected.] ");
+          }
+      }
+      $sql = "SELECT EMAIL, FIRST_NAME, LAST_NAME FROM PASSE_COQUE_MEMBERS;";
+      if ($verbose) {
+          echo('[Performing instruction ['.$sql.']] ');
+      }
+      
+      $result = mysqli_query($link, $sql);
+      if ($verbose) {
+          echo ("Returned " . $result->num_rows . " row(s)<br/>");
+      }
+
+      $members = array();
+      $memberIndex = 0;
+      while ($table = mysqli_fetch_array($result)) { // go through each row that was returned in $result
+          $members[$memberIndex] = new Referent();
+          $members[$memberIndex]->email = $table[0];
+          $members[$memberIndex]->firstName = urldecode($table[1]);
+          $members[$memberIndex]->lastName = urldecode($table[2]);
+          $memberIndex++;
+      }
+      // On ferme !
+      $link->close();
+      if ($verbose) {
+          echo("[Closed DB] ".PHP_EOL);
+          echo "Finally, returning $boats";
+      }
+      return $members;
+
+  } catch (Throwable $e) {
+      echo "[ Captured Throwable for connection : " . $e->getMessage() . "] " . PHP_EOL;
+      throw $e;
+  }                
+  return null;
+}
+
+function populateBoatOptions($boatsArray, $boatId) {
+  foreach ($boatsArray as $boat) {
+    echo '<option value="' . $boat->id . '"' . ($boat->id == $boatId ? ' selected' : '') . '>' . $boat->id . ', ' . $boat->name . ' (' . $boat->type . ') </option>' . PHP_EOL;
+  }
+}
+
+function populateMemberOptions($membersArray, $memberEmail) {
+  foreach ($membersArray as $member) {
+    echo '<option value="' . $member->email . '"' . ($member->email == $memberEmail ? ' selected' : '') . '>' . $member->email. ', ' . $member->firstName . ' ' . strtoupper($member->lastName) . '</option>' . PHP_EOL;
+  }
+}
+
+$create_record = false;
+if (isset($_GET['task'])) {
+  if ($_GET['task'] === 'create') {
+    $create_record = true;
+  }
+}
+
+if (isset($_POST['operation'])) {
+  $operation = $_POST['operation'];
+  if ($operation == 'update' || $operation == 'insert' || $operation == 'delete') { // Then do the update/insert...
+
+    // echo("Update:" . $_POST['update'] . ", Delete:" . $_POST['delete'] . " <br/>" . PHP_EOL); // Two Submit buttons
+
+    try {
+      $ref_email = $_POST['ref-id']; 
+      $boat_id = $_POST['boat-id']; 
+      $telephone = $_POST['telephone']; 
+
+      // $link = mysqli_init();  // Mandatory ?
+    
+      $link = new mysqli($dbhost, $username, $password, $database);
+    
+      if ($link->connect_errno) {
+        echo("Oops, errno:" . $link->connect_errno . "...<br/>");
+        die("Connection failed: " . $conn->connect_error);
+      } else {
+        echo("Connected.<br/>");
+      }
+    
+      if (isset($_POST['update'])) {  // TODO urlencode ?
+
+        // echo ("Update for $email ... <br/>");
+
+        $sql = 'UPDATE REFERENTS ' .
+               'SET TELEPHONE = \'' . ($telephone) . '\' ' .
+                'WHERE EMAIL = \'' . $ref_email . '\' AND BOAT_ID = \'' . $boat_id . '\'';
+        // echo ("Update Stmt: $sql ; <br/>");
+      } else if (isset($_POST['delete'])) {
+        $sql = 'DELETE FROM REFERENTS 
+                WHERE EMAIL = \'' . $ref_email . '\' AND BOAT_ID = \'' . $boat_id . '\'';
+      } else if ($operation === 'insert') {
+        $sql = 'INSERT INTO REFERENTS ' .
+               'VALUES (\'' . $ref_email . '\', \'' . urlencode($boat_id) . '\', \'' . ($telephone) . '\')';
+      } else {
+        echo "What do you ant ?? <br/>" . PHP_EOL;
+        $sql = '';
+      }
+      echo('Performing query <code>' . $sql . '</code><br/>');
+    
+      if (true) { // Do perform ?
+        if ($link->query($sql) === TRUE) {
+          echo "OK. Operation performed successfully<br/>" . PHP_EOL;
+        } else {
+          echo "ERROR executing: " . $sql . "<br/>" . $link->error . "<br/>";
+        }
+      } else {
+        echo "Stby<br/>" . PHP_EOL;
+      }
+    
+      // On ferme !
+      $link->close();
+      echo("Closed DB<br/>".PHP_EOL);
+    } catch (Throwable $e) {
+      echo "Captured Throwable for connection : " . $e->getMessage() . "<br/>" . PHP_EOL;
+    }
+    echo("<hr/>" . PHP_EOL);
+    // echo("Again ? Click <a href='#'>Here</a>.");
+    ?>
+    <form action="./_referents.01.php" method="get">
+      <table>
+        <tr>
+          <td colspan="2" style="text-align: center;"><input type="submit" value="Back to Query Form?"></td>
+        </tr>
+      </table>
+    </form>
+    <?php
+  } else {
+    echo ("Operation $operation ...");
+  }
+} else if (isset($_GET['email']) && isset($_GET['boat-id'])) { // Then display the form to update
+  $ref_id = $_GET['email'];
+  $boat_id = $_GET['boat-id'];
+
+  $boatsArray = getBoats($dbhost, $username, $password, $database, $VERBOSE);
+  $membersArray = getMembers($dbhost, $username, $password, $database, $VERBOSE);
+
+  if ($VERBOSE) {
+    foreach ($boatsArray as $boat) {
+      echo 'Boat: ' . $boat->id . ' [' .  $boat->name . '] (' . $boat->type . ') <br/>' . PHP_EOL;
+    }
+  }
+
+  try {
+    $link = new mysqli($dbhost, $username, $password, $database);
+    
+    if ($link->connect_errno) {
+      echo("Oops, errno:".$link->connect_errno."...<br/>");
+      die("Connection failed: " . $conn->connect_error);
+    } else {
+      echo("Connected.<br/>");
+    }
+  
+    $sql = 'SELECT EMAIL, BOAT_ID, TELEPHONE FROM BOATS_AND_REFERENTS WHERE EMAIL = \'' . $ref_id . '\' AND BOAT_ID = \'' . $boat_id . '\';';
+    //             |      |        |
+    //             0      1        2
+
+    echo('Performing query <code>' . $sql . '</code><br/>');
+        
+    // $result = mysql_query($sql, $link);
+    $result = mysqli_query($link, $sql);
+    echo ("Returned " . $result->num_rows . " row(s)<br/>");
+    if ($result->num_rows > 1) {
+      echo "Bizarre...";
+    }
+    echo("<h2>Row for $ref_id and $boat_id </h2>");
+    ?>
+    <form action="#" method="post">
+      <input type="hidden" name="operation" value="update">
+      <!-- Those 2 below because selects are disabled -->
+      <input type="hidden" name="ref-id" value="<?php echo $ref_id ?>">
+      <input type="hidden" name="boat-id" value="<?php echo $boat_id ?>">
+    <?php
+
+    // Open the form for update. Only telephone can be updated (others are FKs).
+    echo("<form action='#' method='post'>" . PHP_EOL);
+    echo "<table>" . PHP_EOL;
+    while ($table = mysqli_fetch_array($result)) { // go through each row that was returned in $result
+
+      // echo ("Debug: " . $table[7] . "...");
+
+      echo('<tr><td>Referent</td><td><select name="ref-id" disabled>' . PHP_EOL);
+      populateMemberOptions($membersArray, $table[0]);
+      // echo('<option value="' . $table[0] . '">' . $table[0] . '</option>' . PHP_EOL);
+      echo('</select></td></tr>' . PHP_EOL);
+      echo('<tr><td>Boat</td><td><select name="boat-id" disabled>' . PHP_EOL); // value="' . urldecode($table[1]) . '" size="40"> ');
+      populateBoatOptions($boatsArray, $table[1]);
+      echo('</select></td></tr>' . PHP_EOL);
+      echo('<tr><td>Telephone</td><td><input type="text" name="telephone" value="' . $table[2] . '" size="40"></td></tr>' . PHP_EOL);
+    }
+    echo "</table>" . PHP_EOL;
+    ?>
+      <input type="submit" value="Update" name="update"> <input type="submit" value="Delete" name="delete">
+    </form>
+    <?php
+    // On ferme !
+    $link->close();
+
+
+  } catch (Throwable $e) {
+      echo "Captured Throwable for connection : " . $e->getMessage() . "<br/>" . PHP_EOL;
+  }
+} else if ($create_record) {
+  // Display the form to create the record
+  $boatsArray = getBoats($dbhost, $username, $password, $database, $VERBOSE);
+  $membersArray = getMembers($dbhost, $username, $password, $database, $VERBOSE);
+  ?>
+  <form action="#" method="post">
+    <input type="hidden" name="operation" value="insert">
+    <table>
+      <tr>
+        <td>Referent</td>
+        <td>
+          <select name="ref-id">
+          <?php
+            populateMemberOptions($membersArray, '');
+          ?>
+          </select>
+        </td>
+      <tr>
+      <tr>
+        <td>Boat</td>
+        <td>
+          <select name="boat-id">
+            <?php
+              populateBoatOptions($boatsArray, '');
+            ?>
+          </select>
+        </td>
+      <tr>
+      <tr><td>Telephone</td><td><input type="text" name="telephone" size="40" value="unknown"></td><tr>
+    </table>
+    <input type="submit" value="Create" name="create">
+  </form>
+  <?php
+
+} else {
+  // WHAT ??
+  echo "What ??";
+}
+    ?>        
+  </body>        
+</html>
