1a09e9d1e647b638d2e56e30fd48934864b5adba
OlivierLD <olivier@lediouris.net>
2024-04-16 14:19:47 +0200

    Misc

---
+++ web-site/admin/sql/_free_sql.php
@@ -0,0 +1,109 @@
+<html lang="en">
+  <!--
+   ! Free SQL Statement
+   +-->
+  <head>
+    <!--meta charset="UTF-8">
+    <meta charset="ISO-8859-1"-->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Free SQL</title>
+    <style type="text/css">
+      * {
+        font-family: 'Courier New'
+      }
+
+      tr > td {
+        border: 1px solid silver;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>PHP / MySQL. Free SQL</h1>
+
+    <?php
+// phpinfo();
+
+require __DIR__ . "/../../php/db.cred.php";
+
+if (isset($_POST['operation'])) {
+  $operation = $_POST['operation'];
+  if ($operation == 'execute') { // Then do the query
+    try {
+      $sql = $_POST['free-sql']; 
+
+      // $link = mysqli_init();  // Mandatory ?
+    
+      echo("Will connect on ".$database." ...<br/>");
+      $link = new mysqli($dbhost, $username, $password, $database);
+    
+      if ($link->connect_errno) {
+        echo("Oops, errno:".$link->connect_errno."...<br/>");
+        die("Connection failed: " . $conn->connect_error);
+      } else {
+        echo("Connected.<br/>");
+      }
+    
+      echo('Executing <code>' . $sql . '</code><br/>');
+    
+      // $result = mysql_query($sql, $link);
+      $result = mysqli_query($link, $sql);
+
+
+      // echo("<pre>" . $result . "</pre>");
+      var_dump($result);
+
+      echo("<br/>");
+
+      echo("Returned " . $result->num_rows . " row(s)<br/>");
+    
+      echo("<h2>Execution returned:</h2>");
+
+      ?> 
+      <!-- Display result. Query ? Update, insert, delete ? -->
+      <?php
+
+      if (true) {
+        $nbFields = $result->field_count;
+        echo "<table>";
+        while ($table = mysqli_fetch_array($result)) { // go through each row that was returned in $result
+          echo("<tr>" . PHP_EOL);
+          for ($i = 0; $i < $nbFields; $i++) {
+            echo("<td>" . $table[$i] . "</td>" . PHP_EOL);
+          }  
+          echo("</tr>" . PHP_EOL);
+        }
+        echo "</table>";
+      }
+      // On ferme !
+      $link->close();
+      echo("<hr>Closed DB<br/>" . PHP_EOL);
+    } catch (Throwable $e) {
+      echo "Captured Throwable for connection : " . $e->getMessage() . "<br/>" . PHP_EOL;
+    }
+    echo("<hr/>" . PHP_EOL);
+    // echo("Again ? Click <a href='#'>Here</a>.");
+
+    ?>
+    <form action="" method="get">
+      <table>
+        <tr>
+          <td colspan="2" style="text-align: center;"><input type="submit" value="Another one ?"></td>
+        </tr>
+      </table>
+    </form>
+    <?php
+  }
+} else { // Then display the form
+    ?>
+    <form action="" method="post">
+      <input type="hidden" name="operation" value="execute">
+      <textarea id="free-sql" name="free-sql" rows="10" cols="80" placeholder="Your SQL Statement goes here"></textarea>
+      <br/>
+      <input type="submit" value="Execute">
+    </form>
+    <?php
+}  
+    ?>        
+  </body>        
+</html>
