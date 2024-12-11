06d3c4035bf5f9cc52472eebe4220d53d0813528
OlivierLD <olivier@lediouris.net>
2024-12-11 08:40:47 +0100

    Cosmetic

--- web-site/tech.and.nav/index.php
+++ web-site/tech.and.nav/index.php
@@ -5,10 +5,23 @@
 <html>
   <head>
     <title>Passe-Coque</title>
-    <link rel="stylesheet" href="../passe-coque.css" type="text/css" id="styleLink">
+    <!--link rel="stylesheet" href="../passe-coque.css" type="text/css" id="styleLink"-->
+    <style type="text/css">
+		* {
+			font-family: 'Courier New', Courier, monospace;
+		}
+        td {
+            border: 1px solid black;
+            border-radius: 5px;
+            padding: 5px;
+        }
+    </style>
   </head>
   <body>
     <h1>Index Passe-Coque, navigation, calculs, techniques, etc</h1>
+    <p style="font-style: italic;">
+      This is a workbench. Sexy UI is <b>not</b> in the scope...
+    </p>
     <?php
 /**
  * What's wrong with a comment ?
@@ -29,12 +42,12 @@ try {
     <h2>Misc links and stuff</h2>
     <ul>
       <li><a href="./astro.php/index.01.html" target="_new">Astro PHP, first POC</a></li>
-      <li><a href="./astro.php/almanac.publisher.html" target="_new">Almanac Publisher (WiP)</a></li>
+      <li><a href="./astro.php/almanac.publisher.html" target="_new">Celestial Almanac Publisher (WiP)</a> <i>Close to prod</i></li>
       <li>
         Tides in PHP
         <ul>
           <li><a href="./tides.php/tide.sample.php" target="_new">Tide Workbench (WiP)</a></li>
-          <li><a href="./tides.php/tide.publisher/tide.publisher.101.php" target="_new">Tide Almanac Publisher. WiP.</a></li>
+          <li><a href="./tides.php/tide.publisher/tide.publisher.101.php" target="_new">Tide Almanac Publisher (WiP).</a> <i>Close to prod</i></li>
         </ul>
       </li>
       <li>
@@ -43,6 +56,14 @@ try {
           <li><a href="https://olivierld.github.io/web.stuff/tides/Basic.04.html" target="_new">Basic test, with Graph</a></li>
           <li><a href="https://olivierld.github.io/web.stuff/tides/earth.tide.stations.html" target="_new">Earth Tide Stations</a></li>
           <li><a href="https://olivierld.github.io/web.stuff/tides/leaflet.tide.stations.html" target="_new">Leaflet Tide Stations</a></li>
+        </ul>
+      </li>
+      <li>
+        Meteo &amp; Co
+        <ul>
+          <li><a href="https://olivierld.github.io/web.stuff/boat.stuff/weather/internet.faxes.NAtl.html" target="OlivLD">Faxes, North Atlantic (real time)</a></li>
+          <li><a href="https://olivierld.github.io/web.stuff/boat.stuff/weather/internet.faxes.NPac.html" target="OlivLD">Faxes, North Pacific (real time)</a></li>
+          <li><a href="https://olivierld.github.io/web.stuff/boat.stuff/weather/forecast.html" target="OlivLD">Several GRIB-based sites</a></li>
         </ul>
       </li>
     </ul>
