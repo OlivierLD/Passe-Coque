c4815a152300e596281cb02000410b297454afa5
OlivierLD <olivier@lediouris.net>
2025-02-14 20:33:03 +0100

    Updates

--- web-site/php/db.cred.php
+++ web-site/php/db.cred.php
@@ -1,15 +1,15 @@
 <?php
 
 // On OVH
-// $username = "passecc128";
-// $password = "zcDmf7e53eTs";
-// $database = "passecc128";
-// $dbhost = "passecc128.mysql.db";
+$username = "passecc128";
+$password = "zcDmf7e53eTs";
+$database = "passecc128";
+$dbhost = "passecc128.mysql.db";
 
 // Locally
-$username = "pc";
-$password = "pc";
-$database = "pc"; // "passecc128";
-$dbhost = "localhost";
+// $username = "pc";
+// $password = "pc";
+// $database = "pc"; // "passecc128";
+// $dbhost = "localhost";
 
 ?>
